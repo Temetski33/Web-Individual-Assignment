108d24e0c5795afe9e7219e129b08afd4a500b10
Temetski33 <teemu.laasio@gmail.com>
2025-11-24 14:11:06 +0200

    Add new logo and more placeholder restaurants

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 // Example restaurants
 const restaurants = [
-  { name: "Pizza Pajatso", lat: 60.1699, lng: 24.9384 },
-  { name: "Sushi Maailma", lat: 60.17, lng: 24.94 },
-  { name: "Burgers of Burg", lat: 60.171, lng: 24.935 },
+  { name: "SODEXO Turbogood", lat: 60.1699, lng: 24.9384 },
+  { name: "Unicafe Steissipaikka", lat: 60.17, lng: 24.94 },
+  { name: "Sodexo At Home", lat: 60.171, lng: 24.935 },
+  { name: "Sodexo Turku", lat: 60.4, lng: 22.24 },
+  { name: "Unicafe Kiuruvesi", lat: 63.6, lng: 26.6 },
+  { name: "Sodexo Jyväskylä 1", lat: 62.25, lng: 25.74 },
+  { name: "Sodexo Jyväskylä 2", lat: 62.25, lng: 25.745 },
+  { name: "Sodexo Jyväskylä 3", lat: 62.25, lng: 25.75 },
+  { name: "Sodexo Jyväskylä 4", lat: 62.25, lng: 25.755 },
+  { name: "Sodexo Jyväskylä 5", lat: 62.25, lng: 25.76 },
+  { name: "Unicafe Rovaniemi", lat: 66.5, lng: 25.7 },
+  { name: "Unicafe Rollo El Pollo", lat: 66.52, lng: 25.7 },
 ];
 
 // Render restaurants
