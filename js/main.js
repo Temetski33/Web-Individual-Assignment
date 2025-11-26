7d07ff1fd466a17516b45286956f628a875244ef
Temetski33 <teemu.laasio@gmail.com>
2025-11-26 15:42:07 +0200

    Remove commented out code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import {getRestaurants} from "./restaurants.js";
-import {renderRestaurants} from "./restaurants.js";
+import { getRestaurants } from "./restaurants.js";
+import { renderRestaurants } from "./restaurants.js";
 
 // Open dialog on button click
 loginRegisterButton.addEventListener("click", () => {
@@ -20,22 +20,6 @@ loginButton.addEventListener("click", () => {
 const restaurants = getRestaurants();
 
 renderRestaurants();
-
-/*
-// Render restaurants
-const listEl = document.getElementById("restaurantList");
-restaurants.forEach((r) => {
-  const item = document.createElement("div");
-  item.className = "restaurant";
-  item.textContent = r.name;
-  // Show restaurant on map
-  item.addEventListener("click", () => {
-    map.setView([r.lat, r.lng], 14);
-    if (r._marker) r._marker.openPopup();
-  });
-  listEl.appendChild(item);
-});
-*/
 
 // Initialize map
 const map = L.map("map").setView([60.1699, 24.9384], 14);
