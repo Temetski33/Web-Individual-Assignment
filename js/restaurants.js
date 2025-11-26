e351f297c11cfd001894dc8115e0945136e63954
Temetski33 <teemu.laasio@gmail.com>
2025-11-26 12:10:54 +0200

    Move restaurant creation to separate js file, update html to match structure

---
+++ js/restaurants.js
@@ -0,0 +1,25 @@
+// Example restaurants
+const getRestaurants = () => {
+  const restaurants = [
+    { name: "SODEXO Turbogood", lat: 60.1699, lng: 24.9384 },
+    { name: "Unicafe Steissipaikka", lat: 60.17, lng: 24.94 },
+    { name: "Sodexo At Home", lat: 60.171, lng: 24.935 },
+    { name: "Sodexo Turku", lat: 60.4, lng: 22.24 },
+    { name: "Unicafe Kiuruvesi", lat: 63.6, lng: 26.6 },
+    { name: "Sodexo Jyväskylä 1", lat: 62.25, lng: 25.74 },
+    { name: "Sodexo Jyväskylä 2", lat: 62.25, lng: 25.745 },
+    { name: "Sodexo Jyväskylä 3", lat: 62.25, lng: 25.75 },
+    { name: "Sodexo Jyväskylä 4", lat: 62.25, lng: 25.755 },
+    { name: "Sodexo Jyväskylä 5", lat: 62.25, lng: 25.76 },
+    { name: "Unicafe Rovaniemi", lat: 66.5, lng: 25.7 },
+    { name: "Unicafe Rollo El Pollo", lat: 66.52, lng: 25.7 },
+    { name: "Studencafe Jyväskylä 1", lat: 62.245, lng: 25.74 },
+    { name: "Studencafe Jyväskylä 2", lat: 62.245, lng: 25.745 },
+    { name: "Studencafe Jyväskylä 3", lat: 62.245, lng: 25.75 },
+    { name: "Studencafe Jyväskylä 4", lat: 62.245, lng: 25.755 },
+    { name: "Studencafe Jyväskylä 5", lat: 62.245, lng: 25.76 },
+  ];
+  return restaurants;
+};
+
+export default getRestaurants;
