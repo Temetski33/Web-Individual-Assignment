ed5bf3a7f5d57c280f7d4de6a80839912e7257c6
Temetski33 <teemu.laasio@gmail.com>
2025-12-04 14:52:30 +0200

    Add coordinates to api test

--- js/api/fetchRestaurants.js
+++ js/api/fetchRestaurants.js
@@ -3,9 +3,10 @@ const fetchRestaurants = async () => {
     const resp = await fetch(
       'https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants'
     );
-    if (!resp.ok) throw new Error('HTTP ' + resp.status);
     const restaurants = await resp.json();
     console.log('first name:', restaurants[0]?.name);
+    console.log('lat:', restaurants[0]?.location.coordinates[1]);
+    console.log('lng:', restaurants[0]?.location.coordinates[0]);
     return restaurants;
   } catch (err) {
     console.error('fetch failed:', err);
